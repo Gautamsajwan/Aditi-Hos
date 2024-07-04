72babffaf45b03b9dfc0c2ec9c6498f3797e1408
Ayush Chamola <ayushchamola8433@gmail.com>
2024-07-04 13:59:07 +0530

    Pushed latest changes 04/07/2024

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,13 @@
+// Layout.js
+import React from 'react';
+import Header from './Header';
+import Router from './routes/Routers';
+function Layout() {
+  return (
+    <>
+        <Router></Router> 
+    </>
+  );
+}
+
+export default Layout;
